d285d78bb2221e5ee56f29ce030439b1514f4259
YEJI RYOO <emilyryoo5307@gmail.com>
2023-11-29 04:17:03 +0900

    connection response

--- src/main/java/com/sbb/flexrate/MainController.java
+++ src/main/java/com/sbb/flexrate/MainController.java
@@ -3,19 +3,22 @@ package com.sbb.flexrate;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/main")//swagger 임시 main
-@Api(tags = "Main Controller")
+@RequestMapping("/main")//aws 임시 main
 public class MainController {
-
-    @GetMapping("/hello")
-    @ApiOperation(value = "간단한 테스트API",notes = "swagger 테스트")
-    public String hello(){
-        return "작동!!";
+    @GetMapping
+    public ResponseEntity<String> handleMainRequest() {
+        try {
+            return new ResponseEntity<>("Success", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
